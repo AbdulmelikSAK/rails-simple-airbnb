a58479a23804560dd92d50555db35eb08297bd62
Abdulmelik SAK <melik.sak@icloud.com>
2020-11-11 15:21:30 +0100

    fonctionnalite new,create, show et index OK mais mocheé

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroy all flats'
+
+Flat.destroy_all
+
+puts 'create 4 new flat'
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Elysee',
+  address: '10 rue des champs elysee',
+  description: 'A lovely place with a lovely guy',
+  price_per_night: 750,
+  number_of_guests: 1
+)
+Flat.create!(
+  name: 'My house',
+  address: '620 rue de belleroche',
+  description: 'its my house',
+  price_per_night: 7500,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Appartment in lyon',
+  address: 'place des terreaux lyon',
+  description: 'A good place to have some fun',
+  price_per_night: 60,
+  number_of_guests: 13
+)
+
+puts 'Finish!'
